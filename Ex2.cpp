e1164df30f239c6a5ca96dfa62b8c0de5d0b96c8
TahminaM <73051805+TahminaM@users.noreply.github.com>
2022-09-17 18:24:45 -0400

    Started Coding

---
+++ Ex2.cpp
@@ -0,0 +1,32 @@
+// if (s.length() != t.length()){return false;}
+//     sort(s.begin(), s.end());
+//     sort(t.begin(), t.end());
+
+//     if (s == t){return true;}
+//     else{return false;}
+
+
+/*
+    Given 2 strings, return true if anagrams (same letters diff order)
+    Ex. s = "anagram" & t = "nagaram" -> true, s = "rat" & t = "car" -> false
+
+    Count chars, strings should have same # of chars if anagram
+
+    Time: O(n)
+    Space: O(26)
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if (s.length() != t.length()){return false;}
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if (s == t){return true;}
+        else{return false;}
+
+    }
+};
