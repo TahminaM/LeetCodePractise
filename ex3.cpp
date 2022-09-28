ea984fa8cabdf83aaa811f3d90a1634c93431fb6
TahminaM <Tahminamunni07@gmail.com>
2022-09-27 21:26:30 -0400

    Added two sum

--- ex3.cpp
+++ ex3.cpp
@@ -1,11 +1,37 @@
 
-View on Github
+
 /*
-    Given 2 strings, return true if anagrams (same letters diff order)
-    Ex. s = "anagram" & t = "nagaram" -> true, s = "rat" & t = "car" -> false
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-    Count chars, strings should have same # of chars if anagram
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-    Time: O(n)
-    Space: O(26)
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
+
+
+class Solution {
+public:
+    vector<int> twoSum (vector<int>& nums, int target) {
+        
+        vector<int> answer;
+        
+        for (int i=0; i<nums.size(); i++){
+            for (int j=i+1; j<nums.size(); j++){
+                if (nums[i]+nums[j]== target){
+                    answer = {i,j};
+                    return answer; 
+                }
+            }
+        }
+        
+       return answer; 
+    }
+};
